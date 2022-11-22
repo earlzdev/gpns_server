5ab8b3af15b3e96a702e1454133b711110825f0d
earl <esinilyadev@gmail.com>
2022-11-22 13:02:11 +0500

    Initial commit

---
+++ src/main/kotlin/ru/earl/Application.kt
@@ -0,0 +1,16 @@
+package ru.earl
+
+import io.ktor.server.application.*
+import ru.earl.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
